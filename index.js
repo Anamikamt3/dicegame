6c25fdbfab62d62cb9d104b06d67238b78ab60d6
Anamikamt3 <anamikamt53@gmail.com>
2024-08-29 21:59:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var randomNumber1=Math.floor(Math.random()*6)+1;
+/*var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomDiceImage="dice"+randomNumber1+".png";
 var randomImageSource="images/"+randomDiceImage;
 var image1=document.querySelectorAll("img")[0];
@@ -20,3 +20,32 @@ else
 {
     document.querySelector("h1").innerHTML="Draw!";
 }
+*/
+function rollDice(){
+    var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomDiceImage="dice"+randomNumber1+".png";
+var randomImageSource="images/"+randomDiceImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImageSource2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="🚩Player 1 wins!";
+}
+else if(randomNumber2>randomNumber1)
+{
+    document.querySelector("h1").innerHTML="🚩Player 2 wins!";
+}
+else
+{
+    document.querySelector("h1").innerHTML="Draw!";
+}
+
+}
+    const btn=document.querySelector("btn");
+    btn.addEventListener("click me",rollDice);
+
